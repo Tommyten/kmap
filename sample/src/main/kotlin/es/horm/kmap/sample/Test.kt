8912ad00cdc2ae1301ff4363df8feaed0b99fc62
Tommyten <thomas@horm.es>
2025-08-01 14:37:05 +0200

    - add Aggregator annotation - add First TODO to readme - properly add imports to the generated file by using %T instead of %L for the Transformers Type

--- sample/src/main/kotlin/es/horm/kmap/sample/Test.kt
+++ sample/src/main/kotlin/es/horm/kmap/sample/Test.kt
@@ -1,16 +1,22 @@
+package es.horm.kmap.sample
 
 import com.example.generated.toTestModel
 import es.horm.kmap.runtime.KmapTo
 import es.horm.kmap.runtime.KmapTransformer
 import es.horm.kmap.runtime.Mapping
 
-class cConverter : KmapTransformer<Int, Int> {
+class ConverterForC : KmapTransformer<Int, Int> {
     override fun transform(value: Int): Int = value * 4
+}
+
+class DtoSumAggregator : KmapTransformer<Dto, Int> {
+    override fun transform(value: Dto): Int = with(value) { a + b + c}
 }
 
 @KmapTo(
     target = BusinessModel::class,
-    mappings = [Mapping("c", "test", cConverter::class)]
+    mappings = [Mapping("c", "test", ConverterForC::class)],
+    //aggregators = [Aggregator("sum", DtoSumAggregator::class)]
 )
 @KmapTo(TestModel::class)
 data class Dto(
@@ -23,6 +29,7 @@ data class BusinessModel(
     val a: Int,
     val b: Int,
     val test: Int,
+   // val sum: Int
 )
 data class TestModel(
     val b: Int,
